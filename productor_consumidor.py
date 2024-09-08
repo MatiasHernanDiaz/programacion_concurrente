7600d06767f3cee39c8eaf74ecfd89db94d5fcdd
Diaz Matias Hernan <diaz.matiash@gmail.com>
2024-09-08 20:09:24 -0300

    se termina productor consumidor y espera

--- productor_consumidor.py
+++ productor_consumidor.py
@@ -1,6 +1,7 @@
 #Esquema de UN productor y UN consumidor
 #ABABABAB
 from threading import Thread,Semaphore
+import time 
 """
 
 #Crear variable semáforo
@@ -62,7 +63,11 @@ print()
 print()
 
 """
+
+"""
 ################################################
+#1. Describir en pseudocódigo una solución al problema de productor-consumidor, para el caso de 2 productores y 1 consumidor, 
+#todos sobre un mismo buffer. Escribir todas las aclaraciones que sean necesarias.
 #PRODUCTOR CONSUMIDOR PRODUCTOR
 #ABCBABCB
 
@@ -103,7 +108,8 @@ class NewHilo(Thread):
         #de la zona critica despetara el semaforo del productor sumando 1 al 
         #contador interno del mismo
         i = 0
-        while(i < 10):
+        j = 0
+        while(i < 10 and j < 20):
             if(self.name == 'productor'):
                 n.acquire()
                 x.acquire()
@@ -115,7 +121,7 @@ class NewHilo(Thread):
             if(self.name == 'consumidor'):
                 y.acquire()
                 region_critica(self.name)
-                i += 1
+                j += 1
                 x.release()
 
             if(self.name == 'productor2'):
@@ -126,6 +132,7 @@ class NewHilo(Thread):
                 y.release()
                 n.release()
         print(f"fin {self.name}")
+        exit()
       
         
 
@@ -138,3 +145,60 @@ for h in hilosPPC:
 for h in hilosPPC:
     h.join()
 
+"""
+"""
+# 2. En pseudocódigo, usando semáforos, resolver el problema del "rendez-vous" (o encuentro). 
+# Consiste en tener dos procesos, tales que uno de ellos mientras ejecuta deberá alcanzar un punto 
+# (o marca) dentro de su código, y lo mismo con otro proceso. El primero de ambos que llegue a la marca correspondiente 
+# deberá quedarse esperando a que el otro proceso llegue a su marca, y recién en el momento en que el otro haya llegado, 
+# ambos podrán continuar ejecutando su código a partir de allí. Escribir o discutir luego una solución análoga del 
+# rendez-vous para para 3 o más procesos, cada uno con su código y su marca dada. (Opcionalmente, implementarlo en 
+# Python u otro lenguaje, imprimiendo mensajes que informen cuando los procesos lleguen a las marcas dadas. 4                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            En este caso, dentro del código podrían utilizar algún paquete o clase existente que implemente semáforos...)
+
+# Creación de los semáforos con valor inicial en 0
+x = Semaphore(0)
+y = Semaphore(0)
+
+def proceso_1():
+    print("Proceso 1: ejecutando primera parte")
+    time.sleep(1)  # Simula trabajo
+    print("Proceso 1: alcanzó el punto de encuentro")
+    
+    # Notificar al proceso 2 que proceso 1 llegó al punto de encuentro
+    x.release()
+    
+    # Esperar a que proceso 2 llegue al punto de encuentro
+    y.acquire()
+    
+    print("Proceso 1: ambos procesos llegaron, continuando ejecución")
+
+def proceso_2():
+    print("Proceso 2: ejecutando primera parte")
+    time.sleep(2)  # Simula trabajo
+    print("Proceso 2: alcanzó el punto de encuentro")
+    
+    # Notificar al proceso 1 que proceso 2 llegó al punto de encuentro
+    y.release()
+    
+    # Esperar a que proceso 1 llegue al punto de encuentro
+    x.acquire()
+    
+    print("Proceso 2: ambos procesos llegaron, continuando ejecución")
+
+# Crear hilos para los procesos
+t1 = Thread(target=proceso_1)
+t2 = Thread(target=proceso_2)
+
+# Iniciar los hilos
+t1.start()
+t2.start()
+
+# Esperar que los hilos terminen
+t1.join()
+t2.join()
+
+#Si quisiera tener mas procesos deberia agregar mas semaforos y que condicionen
+#de manera cruzada al resto de los procesos para que esperen a todos. una vez que todos
+#llegen se despiertan los semaforos y continuan
+
+"""
